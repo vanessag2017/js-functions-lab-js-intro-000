0852a4e051fd67823354a2943e875d3d5a6eb077
vanessag217 <vanessagarcia61399@gmail.com>
2016-11-01 18:00:42 +0000

    I'm ready to send my files to github.

---
+++ index.js
@@ -0,0 +1,12 @@
+var happyHolidays = function(){
+  return "Happy holidays!";
+};
+var happyHolidaysTo = function(name){
+  return `Happy holidays, ${name}!`;
+};
+var happyHolidayTo = function(holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+};
+var holidayCountdown = function(holiday, days){
+  return `It's ${days} days until ${holiday}!`;
+};
